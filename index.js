1eeb542bd6aab1cdaf6dd9eb86271b0981e84a9a
semperry <ryan.curtis88@gmail.com>
2020-05-23 22:05:26 -0600

    sync transactions on connect

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.get("/api/transaction-pool-map", (req, res) => {
 	res.json(transactionPool.transactionMap);
 });
 
-const syncChains = () => {
+const syncWithRootState = () => {
 	request(
 		{ url: `${ROOT_NODE_ADDRESS}/api/blocks` },
 		(error, response, body) => {
@@ -67,6 +67,21 @@ const syncChains = () => {
 
 				console.log("replace chain on a sync with", rootChain);
 				blockchain.replaceChain(rootChain);
+			}
+		}
+	);
+
+	request(
+		{ url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map` },
+		(error, response, body) => {
+			if (!error && response.statusCode === 200) {
+				const rootTransactionPoolMap = JSON.parse(body);
+
+				console.log(
+					"replace transaction pool map on a sync with",
+					rootTransactionPoolMap
+				);
+				transactionPool.setMap(rootTransactionPoolMap);
 			}
 		}
 	);
@@ -82,6 +97,6 @@ const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
 	console.log(`listening at localhost:${PORT}`);
 	if (PORT !== DEFAULT_PORT) {
-		syncChains();
+		syncWithRootState();
 	}
 });
