6c4c0ccc9d6fcdec121d8c9fc702184b3d68fd46
semperry <ryan.curtis88@gmail.com>
2020-05-23 21:23:29 -0600

    api endpoint to create transaction

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const request = require("request");
 const Blockchain = require("./blockchain");
 const PubSub = require("./app/pubsub");
+const TransactionPool = require("./wallet/transaction-pool");
+const Wallet = require("./wallet");
 
 const app = express();
 const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain });
 
 const DEFAULT_PORT = 3000;
@@ -24,6 +28,18 @@ app.post("/api/mine", (req, res) => {
 	pubsub.broadcastChain();
 
 	res.redirect("/api/blocks");
+});
+
+app.post("/api/transact", (req, res) => {
+	const { amount, recipient } = req.body;
+
+	const transaction = wallet.createTransaction({ recipient, amount });
+
+	transactionPool.setTransaction(transaction);
+
+	console.log("transactionPool", transactionPool);
+
+	res.json({ transaction });
 });
 
 const syncChains = () => {
