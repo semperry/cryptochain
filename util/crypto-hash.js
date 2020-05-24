3e786cbe2d69c424483a70b1703013beab96d32d
semperry <ryan.curtis88@gmail.com>
2020-05-23 20:57:19 -0600

    transactions validation tests and functionality

--- util/crypto-hash.js
+++ util/crypto-hash.js
@@ -3,7 +3,12 @@ const crypto = require("crypto");
 const cryptoHash = (...inputs) => {
 	const hash = crypto.createHash("sha256");
 
-	hash.update(inputs.sort().join(" "));
+	hash.update(
+		inputs
+			.map((input) => JSON.stringify(input))
+			.sort()
+			.join(" ")
+	);
 
 	// Digest is a term in cryptography that means the result
 	return hash.digest("hex");
--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -58,4 +58,118 @@ describe("Transaction", () => {
 			).toBe(true);
 		});
 	});
+
+	describe("validTransaction()", () => {
+		let errorMock;
+
+		beforeEach(() => {
+			errorMock = jest.fn();
+
+			global.console.error = errorMock;
+		});
+
+		describe("when the transaction is valid", () => {
+			it("returns true", () => {
+				expect(Tranaction.validTransaction(transaction)).toBe(true);
+			});
+		});
+
+		describe("when the transaction is invalid", () => {
+			describe("and a transaction outputMap is invalid", () => {
+				it("returns false and logs an error", () => {
+					transaction.outputMap[senderWallet.publicKey] = 99999;
+
+					expect(Tranaction.validTransaction(transaction)).toBe(false);
+					expect(errorMock).toHaveBeenCalled();
+				});
+			});
+
+			describe("and a transaction input signature is invalid", () => {
+				it("returns false and logs an error", () => {
+					transaction.input.signature = new Wallet().sign("data");
+
+					expect(Tranaction.validTransaction(transaction)).toBe(false);
+					expect(errorMock).toHaveBeenCalled();
+				});
+			});
+		});
+	});
+
+	describe("update()", () => {
+		let originalSignature, originalSenderOutput, nextRecipient, nextAmount;
+
+		describe("and the amount is invalid", () => {
+			it("throws an error", () => {
+				expect(() => {
+					transaction.update({
+						senderWallet,
+						recipient: "foo",
+						amount: 999999,
+					});
+				}).toThrow("Amount exceeds balance");
+			});
+		});
+
+		describe("and the amount is valid", () => {
+			beforeEach(() => {
+				originalSignature = transaction.input.signature;
+				originalSenderOutput = transaction.outputMap[senderWallet.publicKey];
+				nextRecipient = "next-recipient";
+				nextAmount = 50;
+
+				transaction.update({
+					senderWallet,
+					recipient: nextRecipient,
+					amount: nextAmount,
+				});
+			});
+
+			it("outputs the amount to the next recipient", () => {
+				expect(transaction.outputMap[nextRecipient]).toEqual(nextAmount);
+			});
+
+			it("substracts the amount from the original sender output amount", () => {
+				expect(transaction.outputMap[senderWallet.publicKey]).toEqual(
+					originalSenderOutput - nextAmount
+				);
+			});
+
+			it("maintains a total output that matches the input amount", () => {
+				expect(
+					Object.values(transaction.outputMap).reduce(
+						(total, outputAmount) => total + outputAmount
+					)
+				).toEqual(transaction.input.amount);
+			});
+
+			it("re-signs the transaction", () => {
+				expect(transaction.input.signature).not.toEqual(originalSignature);
+			});
+
+			describe("and another update for the same recipient", () => {
+				let addedAmount;
+
+				beforeEach(() => {
+					addedAmount = 80;
+					transaction.update({
+						senderWallet,
+						recipient: nextRecipient,
+						amount: addedAmount,
+					});
+				});
+
+				it("adds to the recipient amount", () => {
+					expect(transaction.outputMap[nextRecipient]).toEqual(
+						nextAmount + addedAmount
+					);
+				});
+
+				it("subtracts the amount from the original sender output amount", () => {
+					expect(transaction.outputMap[senderWallet.publicKey]).toEqual(
+						originalSenderOutput - nextAmount - addedAmount
+					);
+				});
+			});
+		});
+	});
 });
