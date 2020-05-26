a9e8e24c71bd7aa305851aad4e3cf3133674f860
semperry <ryan.curtis88@gmail.com>
2020-05-26 14:55:05 -0600

    added known addresses to conduct transaction page

--- client/src/components/ConductTransaction.js
+++ client/src/components/ConductTransaction.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FormGroup, FormControl, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
@@ -7,6 +7,14 @@ import history from "../history";
 const ConductTransaction = () => {
 	const [recipient, setRecipient] = useState("");
 	const [amount, setAmount] = useState(0);
+	const [knownAddresses, setKnownAddresses] = useState([]);
+
+	useEffect(() => {
+		fetch(`${document.location.origin}/api/known-addresses`)
+			.then((res) => res.json())
+			.then((data) => setKnownAddresses(data))
+			.catch((err) => console.error(err));
+	}, []);
 
 	const handleSubmit = () => {
 		fetch(`${document.location.origin}/api/transact`, {
@@ -31,6 +39,16 @@ const ConductTransaction = () => {
 		<div className="ConductTransaction">
 			<Link to="/">Home</Link>
 			<h3>Conduct a Transaction</h3>
+			<br />
+			<h4>Known Addresses</h4>
+			{knownAddresses.map((knownAddress) => {
+				return (
+					<div key={knownAddress}>
+						<div>{knownAddress}</div>
+					</div>
+				);
+			})}
+			<br />
 			<FormGroup>
 				<FormControl
 					input="text"
